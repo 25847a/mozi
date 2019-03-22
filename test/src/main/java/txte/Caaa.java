a9e9a446c1dcb724f1ec5f4375ccbd98ed3f9f06
25847a <47727969+25847a@users.noreply.github.com>
2019-03-22 13:42:24 +0800

    4

---
+++ test/src/main/java/txte/Caaa.java
@@ -0,0 +1,10 @@
+package txte;
+
+public class Caaa {
+
+	public static void main(String[] args) {
+		Tecs.add("AAAAA", "BBBBB");
+		System.out.println(Tecs.get("AAAAA"));
+	}
+
+}
---
+++ test/src/main/java/txte/Dsss.java
@@ -0,0 +1,10 @@
+package txte;
+
+public class Dsss {
+
+	public static void main(String[] args) {
+		System.out.println(Tecs.get("AAAAA"));
+
+	}
+
+}
